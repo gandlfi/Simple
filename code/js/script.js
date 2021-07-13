ad2e8b22e8172eaeae838af2fbd255bc63760f71
Luiza Gandolfi <82842048+gandlfi@users.noreply.github.com>
2021-07-13 17:51:28 -0300

    Add files via upload

---
+++ code/js/script.js
@@ -0,0 +1,19 @@
+
+if(window.SimpleSlide){
+  new SimpleSlide({
+    slide: 'clientes',
+    time: 5000, 
+    pauseOnHover: true,
+    nav: true
+  });
+  new SimpleSlide({
+    slide: 'servicos',
+    time: 5000, 
+    pauseOnHover: true,
+    nav: true
+    });
+}
+
+if (window.SimpleAnime) {
+	new SimpleAnime();
+}
